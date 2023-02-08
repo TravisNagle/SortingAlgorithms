e237df102fb7a2768146b1479a8886dca1f5e667
iStratix <itsnagle@gmail.com>
2023-02-08 13:42:21 -0500

    Jagged array, file path, and ReadFile method created

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[][] jaggedArray = new int[20][];
+            string filePath = $"../../../ArrayValues/inputJagged.csv";
+            ReadFile(filePath);
+        }
+
+        public static void ReadFile(string path)
+        {
+            StreamReader sr = new StreamReader(path);
         }
     }
 }
